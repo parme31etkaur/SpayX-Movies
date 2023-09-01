f6da188dd9fbfbdd1ff2dc970f316200dded048c
parmeet <parmeetkaur.3113@gmail.com>
2023-09-02 01:23:12 +0530

    updated url

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ passport.deserializeUser(function(id,done){
 passport.use(new GoogleStrategy({
     clientID:process.env.CLIENT_ID,
     clientSecret:process.env.CLIENT_SECRET,
-    callbackURL:"http://localhost:3000/auth/google/index",
+    callbackURL:"https://spayx-y24j.onrender.com/auth/google/index",
     userProfileURL:'https://www.googleapis.com/oauth2/v3/userinfo'
 },
 function(accessToken, refreshToken, profile, cb){
